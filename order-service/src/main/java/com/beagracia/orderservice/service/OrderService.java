d3dff99834550372a3be661e7fa6d02bb29182a4
whathebea <bea.graciast@gmail.com>
2023-03-26 14:06:45 -0300

    Created OrderService.java with placeOrder method for placing new orders

---
+++ order-service/src/main/java/com/beagracia/orderservice/service/OrderService.java
@@ -0,0 +1,38 @@
+package com.beagracia.orderservice.service;
+
+import com.beagracia.orderservice.dto.OrderLineItemsDto;
+import com.beagracia.orderservice.dto.OrderRequest;
+import com.beagracia.orderservice.model.Order;
+import com.beagracia.orderservice.model.OrderLineItems;
+import com.beagracia.orderservice.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList().stream().map(this::mapToDto).toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
